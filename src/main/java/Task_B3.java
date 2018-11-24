ae580e01dace2f76909df6e2ecc8899bf6e67c14
eatrzcinska <eatrzcinska@gmail.com>
2018-11-24 20:19:16 +0100

    The solution of Task_B3

---
+++ src/main/java/Task_B3.java
@@ -0,0 +1,26 @@
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Task_B3 {
+    public static void main(String[] args) throws IOException, ParseException {
+
+        JSONParser parser = new JSONParser();
+
+        JSONArray jsonArray = (JSONArray) parser.parse(new FileReader("src\\main\\resources\\task_B3.json"));
+
+        for (Object object : jsonArray) {
+
+            JSONObject cryptocurrency = (JSONObject) object;
+
+            String name = (String) cryptocurrency.get("name");
+            String price_usd = (String) cryptocurrency.get("price_usd");
+
+            System.out.println(name + ": " + price_usd);
+        }
+    }
+}
